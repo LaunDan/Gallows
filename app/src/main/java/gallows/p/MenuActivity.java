71dbf58bdf46a1e3e5e9df5c98bf4ab93fc1c840
LaunDan <launadaniel2@gmail.com>
2020-04-25 09:17:46 +0200

    edit code for testing

--- app/src/main/java/gallows/p/MenuActivity.java
+++ app/src/main/java/gallows/p/MenuActivity.java
@@ -2,6 +2,7 @@ package gallows.p;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
@@ -22,5 +23,19 @@ public class MenuActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+        Thread thrdWlcmscrnDelay = new Thread() {
+            public void run() {
+                try {
+                    sleep(4000);
+                    startActivity(new Intent(getApplicationContext(), TopicActivity.class));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    finish();
+                }
+            }
+        };
+        thrdWlcmscrnDelay.start();
     }
 }

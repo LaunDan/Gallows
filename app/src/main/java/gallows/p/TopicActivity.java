e9db5a964a939347506aafdcfc0ebd3a844aa4b3
LaunDan <launadaniel2@gmail.com>
2020-04-26 11:25:29 +0200

    set topic activity and created method play

--- app/src/main/java/gallows/p/TopicActivity.java
+++ app/src/main/java/gallows/p/TopicActivity.java
@@ -4,11 +4,15 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TopicActivity extends AppCompatActivity {
     private View decorView;
+    int met;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +29,25 @@ public class TopicActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 
 
+    }
+
+    public void play(View v) {
+        RadioGroup rg = findViewById(R.id.radioGroup);
+        int choosenID = rg.getCheckedRadioButtonId();
+
+        RadioButton choosenRadio = findViewById(choosenID);
+
+        String chosenTopic = choosenRadio.getText().toString();
+        Toast.makeText(getApplicationContext(), "Chosen topic: " + chosenTopic, Toast.LENGTH_LONG).show();
+
+        if (chosenTopic.equals(" Towns")) met = 1;
+        if (chosenTopic.equals(" States")) met = 2;
+        if (chosenTopic.equals(" Animals")) met = 3;
+        if (chosenTopic.equals(" Cars")) met = 4;
+
+        Intent inte = new Intent(this, MainGameActivity.class);
+        inte.putExtra("ChosenMethod", met);
+        startActivity(inte);
+        finish();
     }
 }

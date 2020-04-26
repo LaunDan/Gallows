79f1f9f9ba9cd98eb1e2ff46fe5382d7eb0b47ca
LaunDan <launadaniel2@gmail.com>
2020-04-26 10:25:14 +0200

    set class to hide task bar

--- app/src/main/java/gallows/p/ShowScoreActivity.java
+++ app/src/main/java/gallows/p/ShowScoreActivity.java
@@ -1,4 +1,27 @@
 package gallows.p;
 
-public class ShowScoreActivity {
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShowScoreActivity extends AppCompatActivity {
+
+    private View decorView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_score);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
+
+        decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
 }
